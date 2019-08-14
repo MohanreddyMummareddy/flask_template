d69f78eb4a4382763cc639083445bb90bd2f6218
Kyle Walker <unixislife@gmail.com>
2019-08-14 16:53:52 -0500

    removed failed debug portion

--- debug.py
+++
@@ -1,9 +0,0 @@
-"""
-Main debugging app starter
-"""
-
-from app import app
-import os
-if __name__ == "__main__":
-    app.secret_key = os.urandom(24)
-    app.run(host='0.0.0.0', debug=True, port=5000)
